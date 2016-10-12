79cc895011c7d0ca8b9f8161762cfbaeec62b0be
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-10-12 15:23:37 -0700

    Added Diagram and main class JDMTuner

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-
+var JDMTuner = function (make, model, engine, owner) {
+    this.make = make;
+    this.model = model;
+    this.engine = engine;
+    this.owner = owner;
+};
 
 function main() {
 
